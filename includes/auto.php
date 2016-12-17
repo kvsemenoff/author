2fb54ae360aa1e5491a4e6c1293b11895ef85b06
Barasbi <betokov93@gmail.com>
2016-12-17 12:13:47 +0300

    round

--- includes/auto.php
+++ includes/auto.php
@@ -7,14 +7,14 @@
 				    <h4>популярных среди угонщиков</h4>
 			    </div>
 			</div>
-    		<div class="col-md-6 col-xs-6 col-lg-6 col-sm-6">
+    		<div class="col-md-6 col-xs-12 col-lg-6 col-sm-6">
  				<div class="dc-count-two">
 					<div class="dc-play">
  					    <a href="#"><img src="img/dc-auto50.png" alt="play" class="img-responsive"><div class="ford">Ford Focus</div><img src="img/dc-round.png" alt="play" class="img-responsive round"></a>
  					</div>    
  				</div>
  		    </div>
-    		<div class="col-md-6 col-xs-6 col-lg-6 col-sm-6">
+    		<div class="col-md-6 col-xs-12 col-lg-6 col-sm-6">
  				<div class="dc-count-two">
 					<div class="dc-play">
  					    <a href="#"><img src="img/dc-auto50.png" alt="play" class="img-responsive"><div class="ford">Ford Focus</div><img src="img/dc-round.png" alt="play" class="img-responsive round"></a>
