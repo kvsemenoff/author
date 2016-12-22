078101ce9e70fa1a54e254b0b13061702400c57d
unknown <anzarsh@mail.ru>
2016-12-22 17:22:08 +0300

    a22

---
+++ includes/forfree.php
@@ -0,0 +1,35 @@
+<div class="df-bg">
+	<div class="container">
+		<div class="row">
+			<div class="df-free">
+				<h3>Что мы можем сделать для Вас БЕСПЛАТНО</h3>
+				<div class="df-two-boxs">
+					<div class="df-box">
+						<div class="df-img">
+							<img src="img/df-img1.png" alt="" class="img-responsive">
+						</div>
+						<span>Консультация по телефону</span>
+						<ul>
+							<li>Рекомендацию противоугонное устройство в соответсвии. с тех. особенностями автомобиля
+							и рисками угона</li>
+							<li>Как получить скидку на КАСКО</li>
+						</ul>
+					</div>
+					<div class="df-box">
+						<div class="df-img">
+							<img src="img/df-img2.png" alt="" class="img-responsive">
+						</div>
+						<span>Консультация в установочном центре</span>
+						<ul>
+							<li>Рекомендацию противоугонное устройство в соотве. с тех. особенностями автомобиля и рисками угона
+							</li>
+							<li>Консультация по тому как получить скидку на КАСКО</li>
+							<li>Подбор уникальных мест установки о которых угонщики и не подозревают</li>
+							<li>Кофе</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
