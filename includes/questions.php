80d3bafdfe5ba000ae1f1a50938622f1cd1f138c
engine77 <zaur.ka.00@mail.ru>
2016-12-22 17:55:12 +0300

    21

--- includes/questions.php
+++ includes/questions.php
@@ -1,1 +1,44 @@
-fff
+<div class="df-bg-2">
+	<div class="container">
+		<div class="row">
+			<div class="df-questions">
+				<h3>Часто задаваемые вопросы</h3>
+				<div class="df-quest">
+					<div class="df-block1">
+						<a href="#">Не угонят</a>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do </p>
+					</div>
+					<div class="df-block1">
+						<a href="#">Сколько времени длится установка?</a>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem</p>
+					</div>
+					<div class="df-block1">
+						<a href="#">Какая гарантия на оборудование</a>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in </p>
+					</div>
+					<div class="df-block1">
+						<a href="#">Не снимут ли мой автомобиль с гарантии?</a>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do abore et dolore magnam aliquam quaerat voluptatem</p>
+					</div>
+					<div class="df-block1">
+						<a href="#">Почему на разные автомобили - разные комплексы?</a>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim pisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem</p>
+					</div>
+					<div class="df-block1">
+						<a href="#">По чему я должен выбрать Ваш установочный центр?</a>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut agni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem</p>
+					</div>				
+				</div>
+				<span class="df-button"><a href="#">Получить консультацию БЕСПЛАТНО</a></span>
+			</div>
+		</div>
+	</div>
+</div>
+<script>
+
+    $('.df-block1 a').click(function() {    
+    	$(this).next('p').slideToggle(500);
+    	$(this).toggleClass("dfgalka");
+    	return false;    	    	
+    });    
+</script>				
