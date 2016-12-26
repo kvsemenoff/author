b2dfe72d44d871feef6eede63ba7594a5d0fe294
unknown <anzarsh@mail.ru>
2016-12-26 14:56:24 +0300

    change history.php

--- includes/yourcar.php
+++ includes/yourcar.php
@@ -1,35 +1,44 @@
 <section class="db-all-yourcar">
-	<div class="db-container-yourcar">
-		<div class="db-h-yourcar">
-			<h3>Сейчас ваш автомобиль под угрозой!</h3>
-		</div>
-	 	<div class="db-all-content">
-		<div class="db-left-yourcar">
-			<div class="db-left-one">
-				<img src="img/db-one.png" alt="one">
+	<div class="container">
+		<div class="row">
+			<div class="db-container-yourcar">
+				<div class="db-h-yourcar">
+					<h3>Сейчас ваш автомобиль под угрозой!</h3>
+				</div>
+				<div class="db-all-content">
+				<div class="col-md-4">
+					<div class="db-left-yourcar">
+						<div class="db-left-one">
+							<img src="img/db-one.png" alt="one">
+						</div>
+						<div class="db-left-two">
+							Занимает Спб<br>
+							среди всех городов РФ<br>
+							по количеству угонов.
+						</div>
+					</div>
+					</div>
+					<div class="col-md-4">
+					<div class="db-middle-yourcar">
+						<img src="img/db-two.png" alt="two">
+						<br>
+						Автомобилей в Спб угнали<br>
+						за 2016 год
+					</div>
+					</div>
+					<div class="col-md-4">
+					<div class="db-right-yourcar">
+						<div class="db-left-one">
+							<img src="img/db-three.png" alt="three">
+						</div>
+						<div class="db-left-two">
+							Автомобиль купленный<br>
+							в автосалоне угоняется<br>
+						</div>
+					</div>
+					</div>
+				</div>
 			</div>
-			<div class="db-left-two">
-				Занимает Спб<br>
-				среди всех городов РФ<br>
-				по количеству угонов.
-			</div>
-		</div>
-		<div class="db-middle-yourcar">
-			<img src="img/db-two.png" alt="two">
-			<br>
-			Автомобилей в Спб угнали<br>
-			за 2016 год
-		</div>
-		<div class="db-right-yourcar">
-			<div class="db-left-one">
-				<img src="img/db-three.png" alt="three">
-			</div>
-			<div class="db-left-two">
-				Автомобиль купленный<br>
-				в автосалоне угоняется<br>
-			</div>
-		</div>
 		</div>
 	</div>
-	<div class="clear"></div>
 </section>
