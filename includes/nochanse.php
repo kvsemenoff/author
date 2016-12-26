80164fc87de3328d3a83a0f42a73c0760ff56915
engine77 <zaur.ka.00@mail.ru>
2016-12-26 11:51:38 +0300

    23

--- includes/nochanse.php
+++ includes/nochanse.php
@@ -1,11 +1,11 @@
-<div class="df-bg-car">
+<div class="df-bg-car df-nochanse">
 	<div class="container df-container">		
-		<div class="df-find-car">
-			<h3>Не дайте шанс злоумышленникам!</h3>
+		<div class="df-find-car ">
+			<h3>Не дайте шанс <br> злоумышленникам!</h3>
 			<h4>Защитите свой автомобиль сегодня!</h4>
 			<span><a href="#">Получить консультацию БЕСПЛАТНО</a></span>
 			<div class="df-car">
-				<img src="img/df-car.png" alt="" class="img-responsive">
+				<img src="img/df-car2.png" alt="" class="img-responsive">
 			</div>
 		</div>
 	</div>	
