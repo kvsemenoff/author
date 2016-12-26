2328a06cf4e48580c79d36b30e3551b6acb8ef3d
unknown <anzarsh@mail.ru>
2016-12-26 09:22:23 +0300

    add history.php

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
 	<?php require_once('includes/yourcar.php'); ?>
 	<?php require_once('includes/slider.php'); ?>
 	<?php require_once('includes/neuspel.php'); ?>
+	<?php require_once('includes/history_one_client.php'); ?>
 	<?php require_once('includes/photos.php'); ?>
 	<?php require_once('includes/command.php'); ?>
 	<?php require_once('includes/forfree.php'); ?>
