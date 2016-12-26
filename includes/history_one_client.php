d0ea6216e17b995a5f5fad44386e6ac930478c8a
unknown <anzarsh@mail.ru>
2016-12-26 13:38:23 +0300

    change history.php

---
+++ includes/history_one_client.php
@@ -0,0 +1,70 @@
+<div class="db-history_content">
+<div class="db-history-h2">
+	<h2>История одного клиента</h2>
+</div>
+<div class="db-history-block db-history-block-first">
+<div class="db-history-block-one">
+	<img src="img/db-direct.png" alt="image" class="img-responsive">
+</div>
+<div class="db-history-block-two">
+	<span class="db-span-one">12:00 - звонок</span><br>
+	<span class="db-span-two">К нам обратился Сергей. Задача: подобрать противоугонную систему для Ауди А4, <br>
+	уложиться необходимо до конца рабочего дня.</span>
+</div>	
+</div>
+<div class="clearfix"></div>
+<div class="db-history-block db-history-block-second">
+<div class="db-history-block-one">
+	<img src="img/db-audi.png" alt="image" class="img-responsive">
+</div>
+<div class="db-history-block-two">
+	<span class="db-span-one">13:00 - визит клиента</span><br>
+	<span class="db-span-two">После консультации по телефону. Сергей приехал в установочный центр. <br>
+	Бюджет ограничивался 50 000 рублями.</span>
+</div>	
+</div>
+<div class="clearfix"></div>
+<div class="db-history-block db-history-block-second">
+<div class="db-history-block-one">
+	<img src="img/db-remont.png" alt="image" class="img-responsive">
+</div>
+<div class="db-history-block-two">
+	<span class="db-span-one">13:30 - обсуждение комплекта</span><br>
+	<span class="db-span-two">После предварительного анализа мастер предложил Сергею комплекс на основе<br> устройства Игла за 41 970 руб.</span>
+
+</div>	
+</div>
+<div class="clearfix"></div>
+<div class="db-history-block db-history-block-fourth">
+<div class="db-history-block-one">
+	<img src="img/db-test.png" alt="image" class="img-responsive">
+</div>
+<div class="db-history-block-two">
+	<span class="db-span-one">15:00 - установка</span><br>
+	<span class="db-span-two">Не теряя времени, мастера приступили к установке устройства. Чтобы выполнить <br> работу в срок, автомобилем одновременно</span>
+</div>	
+</div>
+<div class="clearfix"></div>
+
+<div class="db-history-block db-history-block-fifth">
+<div class="db-history-block-one">
+	<img src="img/db-wait.png" alt="image" class="img-responsive">
+</div>
+<div class="db-history-block-two">
+	<span class="db-span-one">15:00 - ожидание</span><br>
+	<span class="db-span-two">Окончание работ Сергей вместе с женой ожидали в уютном помещении<br>за чашечкой кофе.</span>
+</div>
+	
+</div>
+<div class="clearfix"></div>
+<div class="db-history-block db-history-block-sixth">
+<div class="db-history-block-one">
+	<img src="img/db-ok.png" alt="image" class="img-responsive">
+</div>
+<div class="db-history-block-two">
+	<span class="db-span-one">15:00 - Сдача работ</span><br>
+	<span class="db-span-two">После завершения мы предоставили Сергею пожизненную, финансовую гарантию <br> и техническую поддержку</span>
+</div>	
+</div>
+</div>
+<div class="clearfix"></div>
