c53478be2dbcccda06e680f4e08b2e2e5a63d1c2
Berzeg07 <berezg86@gmail.com>
2016-12-26 12:03:49 +0300

    29.14

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<section class="dd-footer-bottom">
+	<div class="my-container">
+		<div class="dd-logo">
+			<a href="#"><img src="img/dd-logo.png" alt=""></a>
+		</div>
+		<div class="dd-logo-info">
+			<span>
+				ИНН 3954707908374982347&nbsp;&nbsp;    	г. Санкт-Петербург, ул.Громова 3
+			</span>
+		</div>
+		<div class="dd-phone">
+			<span>Есть вопрос? Звоните:</span>
+			<span>+7(812)409-47-66</span>
+			<span>Пн.-Пт. 9:00 - 18:00</span>
+		</div>
+		<div class="dd-head-button-wr">
+			<div class="dd-head-button">
+				<a href="#">Заказать звонок</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
