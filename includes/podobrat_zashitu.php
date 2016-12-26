a7a1e0a19e3c230e9e42fe5e4b14d5e756b5b9b6
unknown <anzarsh@mail.ru>
2016-12-26 14:35:55 +0300

    change history.php 2

--- includes/podobrat_zashitu.php
+++ includes/podobrat_zashitu.php
@@ -1,56 +1,388 @@
-<!-- <section class="dd-zashita-wrap">
-	<div class="my-container">
-		<div class="dd-zashita-title">
-			<span>Подберите защиту для Вашего автомобиля</span>
-		</div>	
-	</div>
-</section> -->
 
-<section>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h3>Пдберите защиту для вашего автомобиля</h3>
+<div class="container wrap-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="tab-wrap">	
+				<div class="tab">
+					<ul>
+						<li><a href="#tab1" class="active">IGLA</a></li>
+						<li><a href="#tab2" class="t2">StarLine</a></li>
+						<li><a href="#tab3" class="t3">Pandora</a></li>
+						<li><a href="#tab4">Призрак</a></li>
+					</ul>
+				</div>
+			</div>	
+		</div>
+		<div class="clearfix"></div>
+	</div>		
+	
+	<div class="relative">
+		<div id="tab1" class="text-1tab df1">
+			<div class="row">
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+
 			</div>
-			<div class="clearfix"></div>
-			<div class="col-md-4">
-				<div  class="az-select az-margin-bottom10">
-					<select name="" id="">
-						<option value="1">Марка</option>
-						<option value="2">Лига Петрова</option>
-						<option value="3">Супер финал</option>
-						<option value="4">Дивизионы</option>
-						<option value="5">АСБ ФЕСТ</option>
-						<option value="6">Матчи звезд</option>
-						<option value="7">3x3</option>
-						<option value="8">Сборная</option>
-						<option value="9">Тренировочный лагерь</option>
-						<option value="9">Разное</option>
-					</select>
+		</div>
+		<div id="tab2" class="text-1tab df1">
+			<div class="row">
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный-2
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3  col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span><br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
-				<div  class="az-select az-margin-bottom10">
-					<select name="" id="">
-						<option value="1">Сезон 2015/2016</option>
-						<option value="2">Сезон 2014/2015</option>
-						<option value="1">Сезон 2013/2014</option>
-						<option value="2">Сезон 2012/2013</option>
-						<option value="1">Сезон 2011/2012</option>
-						<option value="2">Сезон 2010/2011</option>
-						<option value="1">Сезон 2009/2010</option>
-						<option value="2">Сезон 2008/2009</option>
-						<option value="1">Сезон 2007/2008</option>
-					</select>
+		</div>
+		<div id="tab3" class="text-1tab df1">
+			<div class="row">
+				<div class="col-md-3  col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный-3
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
-				<a class="button-1" href="#">Добавить видео</a>
+		</div>
+		<div id="tab4" class="text-1tab df1">
+			<div class="row">
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный-4
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12">
+					<div class="all-one">
+						<div class="first">
+							Минимальный
+						</div>
+						<div class="second">
+							<img src="img/first.png" alt="img" class="img-responsive">
+						</div>
+						<div class="third">
+							Цена с установкой <br>
+							<span class="third-one">11 970<span>руб.</span></span> <br>
+							<span class="third-two">Полное описание</span>		
+						</div>
+						<div class="fourth">
+							<span><a href="">Заказать установку</a></span>
+						</div>
+					</div>
+				</div>
 			</div>
-			<!-- <div class="col-md-3">
-				
-			</div> -->
-			<div class="clearfix"></div>
 		</div>
 	</div>
-</section>
+</div>
+</div>
+
+</div>
+
+</div>
+
+
+<div class="df11"></div>
+<div class="popup">		
+	<div class="all-one ">
+		<h4 class="df2">X</h4>
+		<div class="first">
+			Минимальный
+		</div>
+		<div class="second">
+			<img src="img/first.png" alt="img" class="img-responsive">
+		</div>
+		<div class="third">
+			Цена с установкой <br>
+			<span class="third-one">11 970<span>руб.</span></span> <br>
+			<span class="third-two"><a href="#">Полное описание</a></span>		
+		</div>
+		<div class="fourth">
+			<span><a href="">Заказать установку</a></span>
+		</div>
+	</div>	
+</div>
+
+<div class="clearfix"></div>
+
+<script>
+	$(function(){
+		$('.third-two').click(function () {
+			$('.popup').fadeIn(350);
+			$('.df2').fadeIn(350);
+			$('.df11').css('background-color', 'black').show();
+		});
+		$('.df11').click(function(){
+			$('.popup').fadeOut(500);
+			$(this).fadeOut(500);
+		});
+		$('.df2').click(function(){
+			$('.popup').fadeOut(500);
+			$('.df11').fadeOut(500);
+		});
+	});
+	
+</script>
+<!-- <script src="js/common.js"></script> -->
+<script>
+	$(document).ready(function() {
+		$('#tab1').css('display','block');
+		$('.tab a').click(function(e) {
+			e.preventDefault();
+			$('a').removeClass('active');
+			$(this).addClass('active');
+			var tab = $(this).attr('href');
+			$('.df1').not(tab).css({'display':'none'});
+			$(tab).fadeIn(400);       
+		});
+	});
+</script>
